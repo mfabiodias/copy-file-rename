4652fb54de84a2a25bbd25eb8416a02e7e3a5bae
Fabio Dias <mfabiodias@gmail.com>
2021-02-27 12:06:17 -0300

    Start Project

---
+++ index.js
@@ -0,0 +1,44 @@
+const scandir  = require('scandir').create();
+const fileCopy = require('fs-copy-file-sync');
+const fs = require("fs")
+const oFolder  = "pdf_origem";  
+const dFolder  = "pdf_destino";  
+
+// Executa na leitura de cada arquivo retornado
+scandir.on('file', (file, stats) => {
+
+    fs.mkdir(dFolder, function(err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("New directory successfully created.")
+        }
+    })
+
+    const gfile = file.split("/")[1].split(".");
+    const ofile = dFolder+"/"+removeAcentos(gfile[0])+"."+gfile[1];
+    
+    fileCopy(
+        file, // path/file.ext
+        ofile // renomeia pasta de origem/destino
+    );
+});
+
+// Imprime error se houver
+scandir.on('error', err => console.log(err));
+
+// Executado na conclusão de leitura da lista de arquivos
+scandir.on('end', _ => console.log('Concluído'));
+
+// Função que percorre o diretório en busca de arquivos
+scandir.scan({
+    dir: `./${oFolder}`,
+    recursive: true,
+    filter: /.*/
+});
+
+// Remove acentuação 
+const removeAcentos = str => str.normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace("_", " ")
+    .replace(/[^a-z0-9-\s]/gi,'');
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Copiar, renomear e remover acentos dos seus arquivos.
+
+### A finalidade desse script é remonear arquivos com acentos, removendo toda acentuação e copia-los para um novo diretório, gerando assim uma copia do arquivo com seu nome livre de caracteres especiais. 
+<br>
+
+### Sua utilização é bem simples:
+
+1. Faça o download ou fork deste repositório;
+2. Rode o comando <b><i>git install</i></b> para baixar as dependencias
+3. Renomeie as variaveis "oFolder" e "dFolder" do arquivo "renameFile.js" com as pastas de origem e destino dos seus arquivos para copia-los e remover a centuação das cópias.
+4. Execute o arquivo "renameFile.js"
