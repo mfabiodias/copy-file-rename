25937045dcb56f5c53170cdf3ff78ef1d64c38f0
Fabio Dias <mfabiodias@gmail.com>
2021-02-27 19:15:45 -0300

    Adicionado contador e tempo de execução.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const scandir  = require('scandir').create();
-const fileCopy = require('fs-copy-file-sync');
-const fs       = require("fs");
-const os       = require("os");
-const oFolder  = "pdf_origem";  
-const dFolder  = "pdf_destino"; 
-const osBar    = os.platform() == "win32" ? "\\" : "/"; 
-const debug    = true; 
+const scandir   = require('scandir').create();
+const fileCopy  = require('fs-copy-file-sync');
+const fs        = require("fs");
+const os        = require("os");
+const oFolder   = "pdf_origem";  
+const dFolder   = "pdf_destino"; 
+const osBar     = os.platform() == "win32" ? "\\" : "/"; 
+const perf      = require('execution-time')();
+const debug     = false; 
+let contador    = 0;
+
+// Start no tempo para contagem
+perf.start();
 
 // Executa na leitura de cada arquivo retornado
 scandir.on('file', (file, stats) => {
@@ -30,6 +35,7 @@ scandir.on('file', (file, stats) => {
     } catch(err) {
         console.error(err)
     } finally {
+        contador++;
         fileCopy(file, ofile);
     }
 
@@ -39,7 +45,11 @@ scandir.on('file', (file, stats) => {
 scandir.on('error', err => console.log(err));
 
 // Executado na conclusão de leitura da lista de arquivos
-scandir.on('end', _ => console.log('Concluído'));
+scandir.on('end', _ => {
+    // Stop no tempo de contagem
+    const results = perf.stop();
+    console.log(`\nConcluído! ${contador} arquivo(s) renomeados em ${(results.time/1000).toFixed(2)} segundos(s).`)
+});
 
 // Função que percorre o diretório en busca de arquivos
 scandir.scan({
